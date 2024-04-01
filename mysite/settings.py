fc818b3b0382d824a960beadb90e52caac90c46e
Jose Salvatierra <jslvtr@gmail.com>
2024-04-01 17:00:24 +0100

    Add celery configuration.

--- mysite/settings.py
+++ mysite/settings.py
@@ -132,3 +132,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Celery setup (using redis)
+REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379")
+
+CELERY_BROKER_URL = CELERY_RESULT_BACKEND = REDIS_URL
